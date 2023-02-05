c98eb353d46b8191d73f466de300542b76353503
jj21065 <jj21065@gmail.com>
2023-02-06 00:02:19 +0800

    add main files and cad files

---
+++ CatFeeder/FW/main/main.ino
@@ -0,0 +1,20 @@
+/*
+    This sketch demonstrates how to scan WiFi networks.
+    The API is almost the same as with the WiFi Shield library,
+    the most obvious difference being the different file you need to include:
+*/
+#include <ESP8266WiFi.h>
+
+void setup() {
+  Serial.begin(115200);
+
+  delay(100);
+}
+
+void loop() {
+
+  while(1)
+  {
+    
+  }
+}
